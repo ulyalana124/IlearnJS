57ccf486951b1a7194cdf5dc4652b8bd2dbeb844
Uliana Sadykova <ulanasadikova65@gmail.com>
2021-01-08 23:52:55 +0200

    added cycles and loops

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let numberOfFilms = +prompt("How many moovies did you see?", "");
 
 let personalMovieDB = {
@@ -8,12 +10,27 @@ let personalMovieDB = {
    privat: false
 };
 
-const a = prompt("One of last watched moovies", ""), 
-      b = prompt("How did you apreciate this moovie?", ""),
-      c = prompt("One of last watched moovies", ""), 
-      d = prompt("How did you apreciate this moovie?", "");
 
-personalMovieDB.moovies[a] = b;
-personalMovieDB.moovies[c] = d;
+for (let i=0; i < numberOfFilms; i++){
+   const a = prompt("One of last watched moovies", ""), 
+         b = prompt("How did you apreciate this moovie?", "");
+   if (a != null && a != '' && a.length < 50 && b != null && b != ''){
+      personalMovieDB.moovies[a] = b;
+      console.log("done");
+   } else {
+      console.log('error');
+      i--;
+   }
+   
+}
 
+if (personalMovieDB.count < 10){
+   alert("Quite a few movies have been viewed");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+   alert("You are a classic spectator");
+}else if (personalMovieDB.count >= 30){
+   alert('You are a movie fan');
+}else{
+   alert('Error');
+}
 console.log(personalMovieDB);

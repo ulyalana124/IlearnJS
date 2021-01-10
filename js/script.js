ff125e8526dbe96f8a4b5b617d3ee3f42f853105
Uliana Sadykova <ulanasadikova65@gmail.com>
2021-01-10 20:13:46 +0200

    The code was changed using functions. Added a function that queries your favorite genres and writes them to an object Array

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 'use strict';
 
-let numberOfFilms = +prompt("How many moovies did you see?", "");
+let numberOfFilms;
+
+function start() {
+   numberOfFilms = +prompt("How many moovies did you see?", "");
+
+   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+      numberOfFilms = +prompt("How many moovies did you see?", "");
+   }
+}
+
+start();
 
 let personalMovieDB = {
    count: numberOfFilms,
@@ -10,27 +20,49 @@ let personalMovieDB = {
    privat: false
 };
 
-
-for (let i=0; i < numberOfFilms; i++){
-   const a = prompt("One of last watched moovies", ""), 
-         b = prompt("How did you apreciate this moovie?", "");
-   if (a != null && a != '' && a.length < 50 && b != null && b != ''){
-      personalMovieDB.moovies[a] = b;
-      console.log("done");
-   } else {
-      console.log('error');
-      i--;
+function rememberMyFilms() {
+   for (let i=0; i < numberOfFilms; i++){
+      const a = prompt("One of last watched moovies", ""), 
+            b = prompt("How did you apreciate this moovie?", "");
+      if (a != null && a != '' && a.length < 50 && b != null && b != ''){
+         personalMovieDB.moovies[a] = b;
+         console.log("done");
+      } else {
+         console.log('error');
+         i--;
+      }
+      
    }
-   
 }
 
-if (personalMovieDB.count < 10){
-   alert("Quite a few movies have been viewed");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-   alert("You are a classic spectator");
-}else if (personalMovieDB.count >= 30){
-   alert('You are a movie fan');
-}else{
-   alert('Error');
+rememberMyFilms();
+
+function detectPersonalLevel(){
+   if (personalMovieDB.count < 10){
+      alert("Quite a few movies have been viewed");
+   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+      alert("You are a classic spectator");
+   }else if (personalMovieDB.count >= 30){
+      alert('You are a movie fan');
+   }else{
+      alert('Error');
+   }
 }
-console.log(personalMovieDB);
+
+detectPersonalLevel();
+
+function showMyDB() {
+   if (personalMovieDB.privat === false){
+      console.log(personalMovieDB);
+   }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+   for (let i = 0; i < 3; i++ ) {
+      personalMovieDB.genres[i] = prompt(`Your favorite ganre N${i + 1}`, '');
+   }
+}
+
+writeYourGenres();
